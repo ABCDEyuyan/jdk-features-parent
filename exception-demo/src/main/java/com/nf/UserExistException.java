b075da3388c293175507773adafa38313ee80851
classroom213 <c213@nf.com>
2023-03-22 16:11:17 +0800

    修正自定义异常编写

--- exception-demo/src/main/java/com/nf/UserExistException.java
+++ exception-demo/src/main/java/com/nf/UserExistException.java
@@ -4,6 +4,7 @@ public class UserExistException extends RuntimeException{
     //下面3个是基础的写法
     //1. 默认构造函数：表示只是有个错误，无特别的信息
     public UserExistException() {
+        super();
     }
 
     //2.一个字符串类型的构造函数，表示有异常的描述信息
