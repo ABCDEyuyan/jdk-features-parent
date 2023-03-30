56ea405c8795e2c545ed13913cc1db49fa69d517
classroom213 <c213@nf.com>
2023-03-30 16:26:00 +0800

    增加对dataSource的支持

--- dbutils-lib/src/main/java/com/nf/dbutils/SqlExecutor.java
+++ dbutils-lib/src/main/java/com/nf/dbutils/SqlExecutor.java
@@ -1,5 +1,6 @@
 package com.nf.dbutils;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -9,12 +10,33 @@ import java.sql.Types;
  * 类似于QueryRunner的一个类，客户用此类完成crud
  */
 public class SqlExecutor {
+    private DataSource dataSource;
+
+    /**
+     * 无参的构造函数意味着不使用DataSource
+     */
+    public SqlExecutor() {
+    }
+
+    /**
+     * 意味着要使用DataSource
+     * @param dataSource
+     */
+    public SqlExecutor(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public int update(Connection conn, String sql, Object... params) {
+        return this.update(conn, false, sql, params);
+    }
+
+    public int update( String sql, Object... params) {
+        Connection conn = prepareConnection();
         return this.update(conn, true, sql, params);
     }
 
-    public int update(Connection conn, boolean closeConn, String sql, Object... params) {
+
+    private int update(Connection conn, boolean closeConn, String sql, Object... params) {
         //下面的代码有一个专门的称呼：guard code（守护代码），先写这些guard代码
         if (conn == null) {
             throw new DaoException("要有一个连接");
@@ -27,6 +49,7 @@ public class SqlExecutor {
             if (closeConn) {
                 ResourceCleanerUtils.closeQuietly(conn);
             }
+            throw new DaoException("sql语句不能是空的");
         }
 
         int rows =0;
@@ -37,8 +60,8 @@ public class SqlExecutor {
             //pstmt = this.prepareStatement(conn,sql);
 
             pstmt = conn.prepareStatement(sql);
-            //设置参数
-            //pstmt.setint,pstmt.setString //
+
+            //设置参数//pstmt.setint,pstmt.setString //
             this.fillStatement(pstmt,params);
             rows = pstmt.executeUpdate();
 
@@ -69,5 +92,20 @@ public class SqlExecutor {
         }
 
 
+    }
+
+    /**
+     * 这里搞一个方法的作用是，可以让子类重写此方法，以便对DataSource
+     * 返回的Connection进行一些额外的设置
+     *
+     * protected修饰符是为了给子类重写，不需要搞成public
+     * @return
+     */
+    protected Connection prepareConnection() {
+        try {
+            return  this.dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new DaoException("从dataSource创建Connection失败",e);
+        }
     }
 }
