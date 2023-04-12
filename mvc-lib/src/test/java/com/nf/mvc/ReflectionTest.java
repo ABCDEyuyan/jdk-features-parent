49e58f0b926ff2f7a037ddb95f5fecbd1f7fb6f4
classroom213 <c213@nf.com>
2023-04-12 16:37:06 +0800

    ReflectionTest类里面添加了泛型实参的测试用例

--- mvc-lib/src/test/java/com/nf/mvc/ReflectionTest.java
+++ mvc-lib/src/test/java/com/nf/mvc/ReflectionTest.java
@@ -6,8 +6,12 @@ import javassist.bytecode.LocalVariableAttribute;
 import org.junit.Test;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ReflectionTest {
 
@@ -50,6 +54,7 @@ public class ReflectionTest {
     public void testClassEquals(){
         Class<SomeClass> aClass = SomeClass.class;
         SomeClass instance = new SomeClass();
+        //下面都返回true，在spring中是用==的比较方式
         System.out.println(aClass == instance.getClass());
         System.out.println(aClass.equals(instance.getClass()));
     }
@@ -64,5 +69,31 @@ public class ReflectionTest {
         System.out.println(m1ReturnType == Void.TYPE); //true
         System.out.println(m1ReturnType == void.class);//true
 
+    }
+
+    @Test
+    public void testGenericParameter() throws Exception{
+        Class<SomeClass> aClass = SomeClass.class;
+        Method m2 = aClass.getDeclaredMethod("m2", List.class);
+
+        Parameter parameter = m2.getParameters()[0];
+
+        ParameterizedType parameterizedType = (ParameterizedType) parameter.getParameterizedType();
+        Class<?> genericType = (Class) parameterizedType.getActualTypeArguments()[0];
+        //输出的java.lang.String ,也就是m2方法的参数类型List<String>的中类型实参
+        System.out.println(genericType);
+
+        System.out.println("============");
+        Method m4 = aClass.getDeclaredMethod("m4", Map.class);
+
+        Parameter m4Parameter = m4.getParameters()[0];
+
+        ParameterizedType m4ParameterParameterizedType = (ParameterizedType) m4Parameter.getParameterizedType();
+        Class<?> g1 = (Class) m4ParameterParameterizedType.getActualTypeArguments()[0];
+        Class<?> g2 = (Class) m4ParameterParameterizedType.getActualTypeArguments()[1];
+
+        //分别输出String与Integer
+        System.out.println(g1);
+        System.out.println(g2);
     }
 }
