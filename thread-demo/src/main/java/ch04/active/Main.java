5a028b4b870eb200d0b11d3609c8936035f5b3c7
classroom213 <c213@nf.com>
2023-05-31 16:36:28 +0800

    增加了死锁活锁与饿死的参考文档

---
+++ thread-demo/src/main/java/ch04/active/Main.java
@@ -0,0 +1,19 @@
+package ch04.active;
+
+/**
+ * 线程活跃性：DeadLock(死锁),LiveLock(活锁),Starvation(饿死)
+ * <h3>快速理解</h3>
+ * <ol>
+ *     <li>死锁：2个线程都阻塞不运行了,都等着对方释放锁</li>
+ *     <li>活锁：2个线程都是在运行中的，但条件总是不满足，比如两个人互相让路，总让到同一个边了</li>
+ *     <li>饿死：没有机会得到执行，比如老实排队的人总被人插队</li>
+ * </ol>
+ * <h3>参考资料</h3>
+ * <a href="https://www.baeldung.com/cs/deadlock-livelock-starvation#:~:text=Livelock%20is%20a%20deadlock-like%20situation%20in%20which%20processes,result%20of%20continuous%20resource%20denial%20to%20a%20process.">死锁、活锁、饿死</a>
+ * <a href="https://www.geeksforgeeks.org/deadlock-starvation-and-livelock/">Deadlock, Starvation, and Livelock</a>
+ * <a href="https://www.baeldung.com/java-deadlock-livelock">死锁与活锁的demo</a>
+ * <a href="https://www.geeksforgeeks.org/deadlock-starvation-java/">死锁与饿死的demo(饿死的例子不太好)</a>
+ * <a href="https://www.w3schools.blog/starvation-in-java">饿死的demo</a>
+ */
+public class Main {
+}
