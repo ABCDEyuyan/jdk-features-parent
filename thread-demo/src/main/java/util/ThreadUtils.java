de8a4c46b68fc4102fd047aeb3e24f8f743e57fc
classroom213 <c213@nf.com>
2023-06-06 11:50:07 +0800

    添加了死锁活锁与饿死的例子

---
+++ thread-demo/src/main/java/util/ThreadUtils.java
@@ -0,0 +1,13 @@
+package util;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadUtils {
+    public static void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
