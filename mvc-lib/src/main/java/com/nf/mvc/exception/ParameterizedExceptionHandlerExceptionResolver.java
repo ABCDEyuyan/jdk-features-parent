1372e45d7eea2e8b86c91516f437335e34384f0a
classroom213 <c213@nf.com>
2023-09-13 16:36:41 +0800

    ParameterizedExceptionHandlerExceptionResolver

---
+++ mvc-lib/src/main/java/com/nf/mvc/exception/ParameterizedExceptionHandlerExceptionResolver.java
@@ -0,0 +1,8 @@
+package com.nf.mvc.exception;
+
+/**
+ * TODO:想让其支持多个异常，见MultiExceptionHandlerExceptionResolver，也想让其像handler一直
+ * 支持各种类型的参数，见RequestMappingHandlerAdapter的实现
+ */
+public class ParameterizedExceptionHandlerExceptionResolver extends ExceptionHandlerExceptionResolver{
+}
