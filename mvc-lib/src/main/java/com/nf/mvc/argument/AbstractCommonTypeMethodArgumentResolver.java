807b39d20cbde8031f1be99f7e7ccab7189b13ea
classroom213 <c213@nf.com>
2023-09-14 18:43:44 +0800

    重构了AbstractCommonTypeMethodArgumentResolver

--- mvc-lib/src/main/java/com/nf/mvc/argument/AbstractCommonTypeMethodArgumentResolver.java
+++ mvc-lib/src/main/java/com/nf/mvc/argument/AbstractCommonTypeMethodArgumentResolver.java
@@ -37,13 +37,15 @@ public abstract class AbstractCommonTypeMethodArgumentResolver implements Method
         /* 如果source为null，会返回一个长度为0的空数组，toObjectArray方法并不是一定要调用的，调用可以避免空引用与数组索引越界的异常 */
         Object[] objectArray = ObjectUtils.toObjectArray(source);
 
+        // 关于if else的常见优化方案见https://www.cnblogs.com/jay-huaxiao/p/12586598.html 这里分支不多，保留if else写法显得更清晰，所以没有优化
         if (isScalarType(parameter)) {
             return resolveScalarType(parameter.getParameterType(), Array.getLength(objectArray) == 0 ? null : objectArray[0], parameter);
         } else if (isScalarTypeArray(parameter)) {
-            return resolverScalarTypeArray(parameter.getComponentType(), objectArray, parameter);
+            return resolveScalarTypeArray(parameter.getComponentType(), objectArray, parameter);
         } else if (isScalarTypeList(parameter)) {
             return resolveScalarTypeList(parameter.getFirstActualTypeArgument(), objectArray, parameter);
         }
+
         /* 代码基本不会走到这里，因为supports方法只会对通常的类型进行处理，在上面的几个if else分支都分别进行了处理 */
         return null;
     }
@@ -90,7 +92,7 @@ public abstract class AbstractCommonTypeMethodArgumentResolver implements Method
         return methodParameter.isList() && supportsInternal(methodParameter.getFirstActualTypeArgument());
     }
 
-    private Object resolverScalarTypeArray(Class<?> scalarType, Object[] values, MethodParameter parameter) throws Exception {
+    private Object resolveScalarTypeArray(Class<?> scalarType, Object[] values, MethodParameter parameter) throws Exception {
         /* 如果数据源为null，那么length就=0，那么array就是一个长度为0的数组，并不会进入到循环里面执行真正的参数处理 */
         int length = Array.getLength(values);
         Object array = Array.newInstance(scalarType, length);
