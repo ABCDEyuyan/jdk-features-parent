23409ab950533aa8609e1e5cd30d2cc2d6d88d0d
cjwin <417278483@qq.com>
2023-11-08 21:54:56 +0800

    给HandlerRequestHandlerAdapter添加了说明

--- mvc-lib/src/main/java/com/nf/mvc/HttpRequestHandler.java
+++ mvc-lib/src/main/java/com/nf/mvc/HttpRequestHandler.java
@@ -4,5 +4,5 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public interface HttpRequestHandler {
-    void processRequest(HttpServletRequest req, HttpServletResponse response) throws Exception;
+    void processRequest(HttpServletRequest req, HttpServletResponse resp) throws Exception;
 }
